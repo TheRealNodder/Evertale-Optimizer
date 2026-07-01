f174f865994142434bebe483e0ae718132f96a79
TheRealNodder <josue_preciado25@yahoo.com>
2026-06-30 18:45:42 -0700

    Bump V5 hook loader

--- optimizer-hook.js
+++ optimizer-hook.js
@@ -13,7 +13,7 @@
   "use strict";
 
   if (!global.OptimizerV5LabLoader) {
-    document.write('<script src="./optimizer-v5-lab/optimizer-v5-loader.js?v=2"><\/script>');
+    document.write('<script src="./optimizer-v5-lab/optimizer-v5-loader.js?v=3"><\/script>');
   }
 
   function runOptimizerSafe() {
